5f382f5d1a6a1843aed586412f69d1942ec408a4
Ongkrab <ong.dkrab@gmail.com>
2016-02-07 20:53:53 +0700

    Initial Commit

---
+++ CompressFastaCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CompressFastaCPP
+//
+//  Created by Wongsapat Tangwanidgoon on 2/7/16.
+//  Copyright © 2016 Wongsapat Tangwanidgoon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
